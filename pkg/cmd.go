24c13747ce930e0f16ec79a990edc1475472dc9d
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-12-17 16:00:00 +0800

    Change the default json provider from gitee to github

--- pkg/cmd.go
+++ pkg/cmd.go
@@ -54,7 +54,7 @@ func init() {
 	rootCmd.Flags().StringVar(&serverOptions.Config, "config", "", "config file (default is $HOME/.mirror-proxy.yaml)")
 	rootCmd.Flags().StringVarP(&serverOptions.DefaultProvider, "default-provider", "", "tsinghua",
 		"The default provider of the update center mirror")
-	rootCmd.Flags().StringVarP(&serverOptions.DefaultJSONServer, "default-json-server", "", "https://gitee.com/jenkins-zh/update-center-mirror/raw/master",
+	rootCmd.Flags().StringVarP(&serverOptions.DefaultJSONServer, "default-json-server", "", "https://jenkins-zh.github.io/update-center-mirror",
 		"The default JSON server of the update center mirror")
 
 	rootCmd.Flags().StringVarP(&serverOptions.Host, "host", "", "127.0.0.1",
